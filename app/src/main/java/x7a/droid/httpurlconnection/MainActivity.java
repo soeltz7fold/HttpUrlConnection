7871d819569898c6bd883fb838c83ba4d5bd8781
soeltz7fold <soeltz7fold@gmail.com>
2016-04-19 10:33:44 +0700

    http://private-2efe7-soeltz7fold.apiary-mock.com/questions

--- app/src/main/java/x7a/droid/httpurlconnection/MainActivity.java
+++ app/src/main/java/x7a/droid/httpurlconnection/MainActivity.java
@@ -169,3 +169,81 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
+
+
+//Extra
+//FORMAT: 1A
+//        HOST: http://polls.apiblueprint.org/
+//        # soeltz7fold
+//        Polls is a simple API allowing consumers to view polls and vote in them.
+//        ## Questions Collection [/questions]
+//        ### List All Questions [GET]
+//        + Response 200 (application/json)
+//
+//        {
+//        "users": [
+//        {
+//        "id": 1,
+//        "email": "dwikunto.bayu@kiranatama.com",
+//        "password": "DKJ3288nd",
+//        "token_auth":"34jkj2UDSF09_d",
+//        "created_at":"2015-12-12T17:00:32",
+//        "updated_at":"2015-12-12T17:00:32"
+//        },
+//        {
+//        "id": 2,
+//        "email": "nunak@mail.com",
+//        "password": "021VdnDkdk",
+//        "token_auth":"dflskj-sdfsdf_d",
+//        "created_at":"2015-12-12T17:00:32",
+//        "updated_at":"2015-12-12T17:00:32"
+//        }
+//        ]
+//        }
+//
+//        ### Create a New Question [POST]
+//
+//        You may create your own question using this action. It takes a JSON
+//        object containing a question and a collection of answers in the
+//        form of choices.
+//
+//        + Request (application/json)
+//
+//        {
+//        "question": "Favourite programming language?",
+//        "choices": [
+//        "Swift",
+//        "Python",
+//        "Objective-C",
+//        "Ruby"
+//        ]
+//        }
+//
+//        + Response 201 (application/json)
+//
+//        + Headers
+//
+//        Location: /questions/2
+//
+//        + Body
+//
+//        {
+//        "question": "Favourite programming language?",
+//        "published_at": "2015-08-05T08:40:51.620Z",
+//        "choices": [
+//        {
+//        "choice": "Swift",
+//        "votes": 0
+//        }, {
+//        "choice": "Python",
+//        "votes": 0
+//        }, {
+//        "choice": "Objective-C",
+//        "votes": 0
+//        }, {
+//        "choice": "Ruby",
+//        "votes": 0
+//        }
+//        ]
+//        }
